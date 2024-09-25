ddda26ae2f0c3a7f1aaa20a1128c84d32d2954f8
KaiBlazier <kaiblazier16@gmail.com>
2024-09-24 21:35:27 -0500

    Update fruitsales.py

--- fruitsales.py
+++ fruitsales.py
@@ -1,8 +1,7 @@
 import pandas as pd
-data = {
-    'Apples': [35, 41],
-    'Bananas': [21, 34]
-}
+data = { 'Apples': [35, 41],
+    'Bananas': [21, 34]}
+
 index = ['2017 Sales', '2018 Sales']
 df = pd.DataFrame(data, index=index)
 
